c8cbbca65bf7755da10657dac7f2fcd3ee586733
liy313 <1034386099@qq.com>
2019-08-26 16:03:05 +0800

    Initial Commit

---
+++ KVODemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  KVODemo
+//
+//  Created by JH on 2019/8/26.
+//  Copyright © 2019 JH. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ KVODemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KVODemo
+//
+//  Created by JH on 2019/8/26.
+//  Copyright © 2019 JH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
